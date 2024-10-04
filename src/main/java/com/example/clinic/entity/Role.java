285ec6edfce8472ca51fc66150d080c883e4a62f
ThisAster <freizen034@mail.ru>
2024-10-04 21:32:48 +0300

    feat: add entities for User, Admin, and Role management

---
+++ src/main/java/com/example/clinic/entity/Role.java
@@ -0,0 +1,38 @@
+package com.example.clinic.entity;
+
+import java.util.Collection;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "roles")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role_name", nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles", cascade = CascadeType.ALL)
+    private Collection<Patient> patients;
+
+    @ManyToMany(mappedBy = "roles", cascade = CascadeType.ALL)
+    private Collection<Doctor> doctors;
+
+    @ManyToMany(mappedBy = "roles", cascade = CascadeType.ALL)
+    private Collection<Admin> admins;
+}
