d472ed45dae9e6f71d21ed66f1228e3c23f621d6
ThisAster <freizen034@mail.ru>
2024-10-12 05:47:54 +0300

    create: HeaderUtils

---
+++ src/main/java/com/example/clinic/util/HeaderUtils.java
@@ -0,0 +1,15 @@
+package com.example.clinic.util;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.data.domain.Page;
+
+public class HeaderUtils {
+
+    public static HttpHeaders createPaginationHeaders(Page<?> page) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Total-Count", String.valueOf(page.getTotalElements()));
+        headers.add("Page-Number", String.valueOf(page.getNumber()));
+        headers.add("Page-Size", String.valueOf(page.getSize()));
+        return headers;
+    }
+}
