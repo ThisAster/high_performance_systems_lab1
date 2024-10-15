5c2bab3ee3abfb1c077d145c72b75eef06d5c53a
lutiick <lutiick@gmail.com>
2024-10-15 20:59:52 +0300

    feat: test migration

---
+++ src/test/java/com/example/clinic/controller/doctor/DoctorControllerTest.java
@@ -0,0 +1,46 @@
+package com.example.clinic.controller.doctor;
+
+import com.example.clinic.app.appointment.dto.AppointmentCreationDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class DoctorControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+
+    @Test
+    void createDoctor(@Value("classpath:/doctor/create.json") Resource json) throws Exception {
+
+        mockMvc.perform(post("/api/doctors")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json.getContentAsByteArray())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").exists());
+
+    }
+
+    @Test
+    void getDoctorById() throws Exception {
+        Long doctorId = 1L; // Replace with actual ID
+
+        mockMvc.perform(get("/api/doctors/{id}", doctorId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(doctorId));
+    }
+}
--- src/test/java/com/example/clinic/service/billing/BillingServiceTest.java
+++ src/test/java/com/example/clinic/service/billing/BillingServiceTest.java
@@ -1,131 +1,40 @@
 package com.example.clinic.service.billing;
 
-import com.example.clinic.app.appointment.dto.AppointmentCreationDTO;
 import com.example.clinic.app.appointment.dto.AppointmentTypeDTO;
-import com.example.clinic.app.appointment.entity.Appointment;
 import com.example.clinic.app.appointment.service.AppointmentService;
 import com.example.clinic.app.billing.dto.ConsultationDTO;
 import com.example.clinic.app.billing.dto.InvoiceDTO;
 import com.example.clinic.app.billing.mapper.ConsultationMapper;
 import com.example.clinic.app.billing.mapper.InvoiceMapper;
 import com.example.clinic.app.billing.service.BillingService;
-import com.example.clinic.app.doctor.dto.DoctorDto;
-import com.example.clinic.app.doctor.entity.Doctor;
-import com.example.clinic.app.doctor.mapper.DoctorMapper;
 import com.example.clinic.app.doctor.service.DoctorService;
-import com.example.clinic.app.patient.dto.PatientDto;
-import com.example.clinic.app.patient.entity.Patient;
-import com.example.clinic.app.patient.repository.PatientRepository;
-import com.example.clinic.app.doctor.repository.DoctorRepository;
 import com.example.clinic.app.patient.service.PatientService;
 import com.example.clinic.exception.EntityNotFoundException;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.transaction.annotation.Transactional;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@Sql(value = {
+//    "/appointments/init.sql" Вот тут можно вставить скрипты, но у нас уже есть в миграции
+//}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
 class BillingServiceTest {
 
     @Autowired
     private BillingService billingService;
 
-    @Autowired
-    private PatientService patientService;
-
-    @Autowired
-    private DoctorService doctorService;
-
-    @Autowired
-    private AppointmentService appointmentService;
-
-    @Autowired
-    private ConsultationMapper consultationMapper;
-    private InvoiceMapper invoiceMapper;
 
     @Test
     void testGenerateInvoice() {
-        PatientDto patientDto1 = new PatientDto(
-                1L,
-                "John Doe",
-                LocalDate.of(1965, 4, 20),
-                "johnlover@example.com");
-        PatientDto patientDto2 = new PatientDto(
-                2L,
-                "Sam Watson",
-                LocalDate.of(1980, 5, 20),
-                "samdoctor@example.com");
-
-        patientService.createPatient(patientDto1);
-        patientService.createPatient(patientDto2);
-
-        DoctorDto doctorDto1 = new DoctorDto(
-                1L,
-                "Dr. House",
-                "Cardiologist"
-        );
-
-        DoctorDto doctorDto2 = new DoctorDto(
-                2L,
-                "Dr. Emily Smith",
-                "Dermatologist"
-        );
-
-        doctorService.createDoctor(doctorDto1);
-        doctorService.createDoctor(doctorDto2);
-
-        AppointmentTypeDTO appointmentTypeDTO1 = new AppointmentTypeDTO(
-                1L,
-                "Cardiology Consultation",
-                "Comprehensive heart health consultation",
-                60,
-                new BigDecimal("250.00"),
-                doctorDto1
-        );
-
-        AppointmentTypeDTO appointmentTypeDTO2 = new AppointmentTypeDTO(
-                2L,
-                "Skin Checkup",
-                "Routine skin examination to check for any abnormalities",
-                45,
-                new BigDecimal("180.00"),
-                doctorDto2
-        );
-
-        AppointmentCreationDTO appointmentCreationDTO1 = new AppointmentCreationDTO(
-                LocalDateTime.of(2024, 12, 5, 10, 45),
-                1L,
-                appointmentTypeDTO1.id()
-        );
-
-        AppointmentCreationDTO appointmentCreationDTO2 = new AppointmentCreationDTO(
-                LocalDateTime.of(2024, 11, 5, 10, 45),
-                2L,
-                appointmentTypeDTO2.id()
-        );
-
-        appointmentService.createAppointment(appointmentCreationDTO1);
-        appointmentService.createAppointment(appointmentCreationDTO2);
-
-        List<Long> patientIds = Arrays.asList(patientDto1.id(), patientDto2.id());
+        List<Long> patientIds = List.of(1L, 2L);
 
         InvoiceDTO invoiceDTO = billingService.generateInvoice(patientIds);
 
@@ -136,19 +45,19 @@ class BillingServiceTest {
         }
 
         assertNotNull(invoiceDTO);
-        assertEquals(2, invoiceDTO.getConsultations().size());
+        assertEquals(3, invoiceDTO.getConsultations().size());
 
         ConsultationDTO consultation1 = invoiceDTO.getConsultations().get(0);
         assertEquals("John Doe", consultation1.getPatient().name());
         assertEquals("Dr. House", consultation1.getDoctor().name());
-        assertEquals(250.00, consultation1.getPrice());
+        assertEquals(new BigDecimal(250), consultation1.getPrice());
 
         ConsultationDTO consultation2 = invoiceDTO.getConsultations().get(1);
         assertEquals("Sam Watson", consultation2.getPatient().name());
         assertEquals("Dr. Emily Smith", consultation2.getDoctor().name());
-        assertEquals(180.00, consultation2.getPrice());
+        assertEquals(new BigDecimal(180), consultation2.getPrice());
 
-        assertEquals(430.00, invoiceDTO.getTotalCost());
+        assertEquals(new BigDecimal(430), invoiceDTO.getTotalCost());
     }
 
 
