2054abab7070e5134aadd56ba82e2c8e3fc159da
SamBridgess <ilya.minyaeff@yandex.ru>
2024-10-29 15:55:33 +0300

    add: recipe controller test

--- src/test/java/com/example/clinic/controller/recipe/RecipeControllerTest.java
+++ src/test/java/com/example/clinic/controller/recipe/RecipeControllerTest.java
@@ -2,24 +2,74 @@ package com.example.clinic.controller.recipe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.containers.PostgreSQLContainer;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class RecipeControllerTest {
+class RecipeControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
+    @Test
+    void createRecipe(@Value("classpath:/recipes/create.json") Resource json) throws Exception {
+
+        mockMvc.perform(post("/api/recipes")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json.getContentAsByteArray())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
+    }
 
     @Test
-    void createRecipeTest() throws Exception {
+    void updateRecipe(@Value("classpath:/recipes/update.json") Resource json) throws Exception {
+        Long recipeId = 2L;
 
+        mockMvc.perform(put("/api/recipes/{id}", recipeId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json.getContentAsByteArray())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void deleteRecipe() throws Exception {
+        Long recipeId = 1L;
+
+        mockMvc.perform(delete("/api/recipes/{id}", recipeId))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Recipe with id " + recipeId + " successfully deleted."));
+    }
+
+    @Test
+    void getRecipeById() throws Exception {
+        Long recipeId = 1L; // Replace with actual ID
+
+        mockMvc.perform(get("/api/recipes/{id}", recipeId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(recipeId));
+    }
+
+    @Test
+    void getRecipes() throws Exception {
+        int page = 0;
+        int size = 2;
+
+        mockMvc.perform(get("/api/recipes")
+                        .param("page", String.valueOf(page))
+                        .param("size", String.valueOf(size)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(size));
     }
 }
