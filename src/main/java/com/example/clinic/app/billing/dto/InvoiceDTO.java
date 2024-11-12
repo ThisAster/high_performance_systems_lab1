2ddd00ebb55c43c3c1e071301a03360e4bd33d04
ThisAster <freizen034@mail.ru>
2024-11-12 11:36:47 +0300

    edit: dtos

--- src/main/java/com/example/clinic/app/billing/dto/InvoiceDTO.java
+++ src/main/java/com/example/clinic/app/billing/dto/InvoiceDTO.java
@@ -2,9 +2,6 @@ package com.example.clinic.app.billing.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Data;
 
@@ -16,10 +13,7 @@ import java.util.List;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class InvoiceDTO {
 
-    @NotEmpty(message = "Consultations list cannot be empty")
     private List<ConsultationDTO> consultations;
 
-    @NotNull(message = "Total cost is required")
-    @Positive(message = "Total cost must be a positive value")
     private BigDecimal totalCost;
 }
