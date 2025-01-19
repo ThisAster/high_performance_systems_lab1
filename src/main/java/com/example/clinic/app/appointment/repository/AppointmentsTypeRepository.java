a4d13f77514b57eb840315463ba4a5bd6af3e0fb
lutiick <lutiick@gmail.com>
2025-01-19 19:01:08 +0300

    feat: decompose to microservices

--- src/main/java/com/example/clinic/app/appointment/repository/AppointmentsTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.clinic.app.appointment.repository;
-
-import com.example.clinic.app.appointment.entity.AppointmentsType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppointmentsTypeRepository extends JpaRepository<AppointmentsType, Long> {
-}
